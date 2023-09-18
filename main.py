5100201c285647aece8e0e268fcb016b643471aa
Hoang Lo <hoangvn1509@gmail.com>
2023-09-18 23:58:56 +0700

    https://ailly.livelead.tech/

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ app = FastAPI()
 
 # CORS - Origins
 origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
     "http://localhost:4173",
     "http://localhost:3000",
 "https://aivoice-frontend.server.chatsy.pro",
